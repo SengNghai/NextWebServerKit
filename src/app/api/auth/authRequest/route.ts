edfc6a74bfb0e81108412d598348265e15813c8a
SN <sn@MacBook-Pro.local>
2025-04-30 01:40:58 +0800

    add other api

---
+++ src/app/api/auth/authRequest/route.ts
@@ -0,0 +1,46 @@
+const refreshToken = async (expiredToken: string) => {
+    try {
+      const response = await fetch('/api/refreshToken', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ token: expiredToken }),
+      });
+  
+      const data = await response.json();
+      if (response.ok && data.token) {
+        localStorage.setItem('token', data.token); // 更新 Token
+      }
+      return data.token;
+    } catch (error) {
+      console.error('Error refreshing token:', error);
+    }
+  };
+  
+  export const authRequest = async (url: string, options: RequestInit) => {
+    let token = localStorage.getItem('token');
+  
+    if (!token) {
+      throw new Error('User not authenticated');
+    }
+  
+    options.headers = {
+      ...options.headers,
+      Authorization: `Bearer ${token}`,
+    };
+  
+    const response = await fetch(url, options);
+  
+    // 如果返回 401 未授权，则尝试续约 Token
+    if (response.status === 401) {
+      token = await refreshToken(token);
+      if (token) {
+        options.headers.Authorization = `Bearer ${token}`;
+        return fetch(url, options); // 重试请求
+      }
+    }
+  
+    return response;
+  };
+  
--- src/app/api/user/logout/route.ts
+++ src/app/api/user/logout/route.ts
@@ -1,4 +1,5 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import { connectDb } from '~/lib/databases/mongodb/mongodb';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     // 验证请求方法
@@ -6,7 +7,12 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       return res.status(405).json({ message: 'Method not allowed' });
     }
   
-    //    
+    // 连接数据库
+    const { collection } = await connectDb('expoWebAppDB', 'Users');
+  
+    // 删除 Token
+    await collection.deleteOne({ token: req.body.token });
+  
     res.status(200).json({ message: 'Logout successful' });
   }
   
