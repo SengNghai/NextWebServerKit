821d4399fb67b78aadcdc19542065402944d7517
SN <sn@MacBook-Pro.local>
2025-05-26 01:45:28 +0800

    add pages chat 26-5-2025 AM:1:45

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,5 +1,16 @@
 const config = {
-  plugins: ["@tailwindcss/postcss"],
+  plugins: [
+    "@tailwindcss/postcss",
+    [
+      "postcss-plugin-px2rem",
+      {
+        rootValue: 100, // 根字体大小 (可根据设计稿调整)
+        unitPrecision: 5, // 保留 `rem` 小数位数
+        propList: ["*"], // 所有属性都转换 `px → rem`
+        exclude: /node_modules/, // 排除 `node_modules`
+      },
+    ]
+  ],
 };
 
 export default config;
